3b55c439e96138d7df4c8e7a1f36dee19b8561fd
Rlqmr7 <240382@E0403-09>
2026-02-27 10:31:36 +0900

    Delete処理

--- Server07UDP/main.cpp
+++ Server07UDP/main.cpp
@@ -11,90 +11,81 @@
 #pragma comment(lib, "ws2_32.lib")
 using namespace std;
 
-struct PLAYER_DATA {
-    int x, y, angle, type;
-};
-
-const unsigned short SERVER_PORT = 8888;
-
+struct PLAYER_DATA { int x, y, angle, type; };
+struct PLAYER_INTERNAL { int x, y; chrono::steady_clock::time_point lastSeen; };
 struct ClientInfo {
     sockaddr_in addr;
-    chrono::steady_clock::time_point lastSeen;
-
     bool operator<(const ClientInfo& other) const {
-        if (addr.sin_addr.s_addr != other.addr.sin_addr.s_addr)
-            return addr.sin_addr.s_addr < other.addr.sin_addr.s_addr;
+        if (addr.sin_addr.s_addr != other.addr.sin_addr.s_addr) return addr.sin_addr.s_addr < other.addr.sin_addr.s_addr;
         return addr.sin_port < other.addr.sin_port;
     }
 };
 
+struct Enemy { int id; float baseX, y, speedY, timer; };
+struct EBullet { float x, y; };
+
 int main() {
-    WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
-
+    WSADATA wsaData; WSAStartup(MAKEWORD(2, 2), &wsaData);
     SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
-    sockaddr_in serverAddr = { AF_INET, htons(SERVER_PORT), INADDR_ANY };
+    sockaddr_in serverAddr = { AF_INET, htons(8888), INADDR_ANY };
     bind(sock, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    unsigned long arg = 0x01; ioctlsocket(sock, FIONBIO, &arg);
 
-    unsigned long arg = 0x01;
-    ioctlsocket(sock, FIONBIO, &arg);
-
-    map<ClientInfo, PLAYER_DATA> clients;
-    cout << "--- UDP Shooting Server Started ---" << endl;
-    cout << "Listening on port: " << SERVER_PORT << endl;
+    map<ClientInfo, PLAYER_INTERNAL> clients;
+    vector<Enemy> enemies;
+    vector<EBullet> eBullets;
+    int enemyIdCounter = 1;
 
     while (true) {
-        PLAYER_DATA recvData;
-        sockaddr_in clientAddr;
-        int addrLen = sizeof(clientAddr);
-
-        int ret = recvfrom(sock, (char*)&recvData, sizeof(PLAYER_DATA), 0, (sockaddr*)&clientAddr, &addrLen);
-
-        if (ret > 0) {
-            ClientInfo info = { clientAddr, chrono::steady_clock::now() };
-
-            // 新規クライアントかチェック
-            if (clients.find(info) == clients.end()) {
-                char ipStr[INET_ADDRSTRLEN];
-                inet_ntop(AF_INET, &clientAddr.sin_addr, ipStr, sizeof(ipStr));
-                cout << "[Join] New Client: " << ipStr << ":" << ntohs(clientAddr.sin_port) << endl;
-                cout << "Total clients: " << clients.size() + 1 << endl;
+        PLAYER_DATA recvBuf;
+        sockaddr_in cAddr;
+        int cLen = sizeof(cAddr);
+        while (recvfrom(sock, (char*)&recvBuf, sizeof(PLAYER_DATA), 0, (sockaddr*)&cAddr, &cLen) > 0) {
+            int type = ntohl(recvBuf.type);
+            if (type == 9) { // 敵死亡
+                int targetId = ntohl(recvBuf.x);
+                for (auto it = enemies.begin(); it != enemies.end(); ) {
+                    if (it->id == targetId) it = enemies.erase(it); else ++it;
+                }
             }
-
-            clients[info].x = ntohl(recvData.x);
-            clients[info].y = ntohl(recvData.y);
-            //clients[info].lastSeen = chrono::steady_clock::now(); // 生存時間を更新
+            else {
+                clients[{cAddr}].x = ntohl(recvBuf.x);
+                clients[{cAddr}].y = ntohl(recvBuf.y);
+                clients[{cAddr}].lastSeen = chrono::steady_clock::now();
+            }
         }
 
-        // タイムアウト処理（5秒以上通信がない場合）
+        // タイムアウト処理
         auto now = chrono::steady_clock::now();
         for (auto it = clients.begin(); it != clients.end(); ) {
-            if (chrono::duration_cast<chrono::seconds>(now - it->first.lastSeen).count() > 5) {
-                char ipStr[INET_ADDRSTRLEN];
-                inet_ntop(AF_INET, &it->first.addr.sin_addr, ipStr, sizeof(ipStr));
-                cout << "[Leave] Client Timeout: " << ipStr << endl;
-
-                it = clients.erase(it);
-                cout << "Total clients: " << clients.size() << endl;
-            }
-            else { ++it; }
+            if (chrono::duration_cast<chrono::seconds>(now - it->second.lastSeen).count() > 3) it = clients.erase(it);
+            else ++it;
         }
 
-        // 送信処理
         if (!clients.empty()) {
-            vector<PLAYER_DATA> sendList;
-            for (auto const& c : clients) {
-                PLAYER_DATA netData = { (int)htonl(c.second.x), (int)htonl(c.second.y), 0, 0 };
-                sendList.push_back(netData);
+            if (enemies.size() < 8) enemies.push_back({ enemyIdCounter++, (float)(150 + rand() % 900), -100.0f, 2.0f, (float)(rand() % 100) });
+
+            for (auto it = enemies.begin(); it != enemies.end(); ) {
+                it->y += it->speedY; it->timer += 0.02f;
+                if (rand() % 100 == 0) eBullets.push_back({ it->baseX + sinf(it->timer) * 80.0f, it->y + 40 });
+                if (it->y > 800) it = enemies.erase(it); else ++it;
             }
+            for (auto it = eBullets.begin(); it != eBullets.end(); ) {
+                it->y += 7.0f; // 弾は速めに
+                if (it->y > 800) it = eBullets.erase(it); else ++it;
+            }
+
+            // 送信データ作成
+            vector<PLAYER_DATA> sendList;
+            for (auto const& c : clients) sendList.push_back({ (int)htonl(c.second.x), (int)htonl(c.second.y), 0, (int)htonl(0) });
+            for (auto const& e : enemies) sendList.push_back({ (int)htonl((int)(e.baseX + sinf(e.timer) * 80.0f)), (int)htonl((int)e.y), (int)htonl(e.id), (int)htonl(1) });
+            for (auto const& eb : eBullets) sendList.push_back({ (int)htonl((int)eb.x), (int)htonl((int)eb.y), 0, (int)htonl(2) });
+
             for (auto const& target : clients) {
-                sendto(sock, (char*)sendList.data(), sendList.size() * sizeof(PLAYER_DATA), 0, (sockaddr*)&target.first.addr, sizeof(sockaddr_in));
+                sendto(sock, (char*)sendList.data(), (int)(sendList.size() * sizeof(PLAYER_DATA)), 0, (sockaddr*)&target.first.addr, sizeof(sockaddr_in));
             }
         }
-        this_thread::sleep_for(chrono::milliseconds(10));
+        this_thread::sleep_for(chrono::milliseconds(16)); // 60FPSに合わせる
     }
-
-    closesocket(sock);
-    WSACleanup();
     return 0;
 }
